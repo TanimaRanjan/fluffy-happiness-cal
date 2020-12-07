57c98077bbb9fd3bc0d5bf5ce992fb7f7a2b994a
Tanima Ranjan <tanimaranjan@gmail.com>
2020-12-07 11:37:06 -0500

    Testing 2 views

--- examples/App.js
+++ examples/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Api from './Api'
 import Intro from './Intro.md'
 import { render } from 'react-dom'
@@ -6,7 +6,6 @@ import Layout from 'react-tackle-box/Layout'
 
 import localizer from 'react-big-calendar/lib/localizers/globalize'
 import globalize from 'globalize'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
@@ -14,72 +13,50 @@ import 'react-big-calendar/lib/sass/styles.scss'
 import './styles.scss'
 import './prism.scss'
 import Card from './Card'
-import ExampleControlSlot from './ExampleControlSlot'
-import Basic from './demos/basic'
-import Selectable from './demos/selectable'
-import CreateEventWithNoOverlap from './demos/createEventWithNoOverlap'
-import Cultures from './demos/cultures'
-import Popup from './demos/popup'
 import Rendering from './demos/rendering'
 import CustomView from './demos/customView'
-import Resource from './demos/resource'
-import DndResource from './demos/dndresource'
-import Timeslots from './demos/timeslots'
-import Dnd from './demos/dnd'
-import DndOutsideSource from './demos/dndOutsideSource'
 import Dropdown from 'react-bootstrap/lib/Dropdown'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
 
 const globalizeLocalizer = localizer(globalize)
 
-
 const EXAMPLES = {
-  basic: 'Basic Calendar',
-  // selectable: 'Create events',
-  createEventWithNoOverlap: 'Create events with no-overlap algorithm',
-  // cultures: 'Localization',
-  popup: 'Show more via a popup',
-  rendering: 'Customized Component Rendering',
-  customView: 'Custom Calendar Views',
-
+  rendering: 'Weeky',
+  customView: '3 Day',
 }
 
-const DEFAULT_EXAMPLE = 'basic'
+const DEFAULT_EXAMPLE = 'rendering'
 
-class Example extends React.Component {
-  constructor(...args) {
-    super(...args)
+const Example = () => {
 
-    this.state = {
-      selected: DEFAULT_EXAMPLE,
-    }
+  const [selected, setSelected] = useState(DEFAULT_EXAMPLE)
+  //  const [current, setCurrent] = useState(Rendering)
+
+  let Current = {
+    rendering: Rendering,
+    customView: CustomView,
+  }[selected]
+
+  console.log('Current 1 ', Current)
+  const select = selected => {
+    console.log('Calling select ')
+    setSelected({ selected })
   }
 
-  select = selected => {
-    this.setState({ selected })
-  }
 
-  componentDidMount() {
+
+  useEffect(() => {
+    
     const hash = (window.location.hash || '').slice(1)
-    this.select(hash || DEFAULT_EXAMPLE)
-  }
-
-  render() {
-    let selected = this.state.selected
-    let Current = {
-      basic: Basic,
-      selectable: Selectable,
-      cultures: Cultures,
-      popup: Popup,
+    console.log('useEffect ', hash, DEFAULT_EXAMPLE, window.location.hash)
+    setSelected(hash || DEFAULT_EXAMPLE)
+    Current = {
       rendering: Rendering,
       customView: CustomView,
-      resource: Resource,
-      timeslots: Timeslots,
-      dnd: Dnd,
-      dndresource: DndResource,
-      dndOutsideSource: DndOutsideSource,
-      createEventWithNoOverlap: CreateEventWithNoOverlap,
     }[selected]
+  }, [selected])
+  console.log('Current ', Current)
+
 
     return (
       <div className="app">
@@ -107,10 +84,10 @@ class Example extends React.Component {
                 <Dropdown.Menu>
                   {Object.entries(EXAMPLES).map(([key, title]) => (
                     <MenuItem
-                      active={this.state.selected === key}
+                      active={selected === key}
                       key={key}
                       href={`#${key}`}
-                      onClick={() => this.select(key)}
+                      onClick={() => select(key)}
                     >
                       {title}
                     </MenuItem>
@@ -118,22 +95,18 @@ class Example extends React.Component {
                 </Dropdown.Menu>
               </Dropdown>
             </Layout>
-            <ExampleControlSlot.Outlet />
           </Card>
           <div className="example">
-            <Current localizer={globalizeLocalizer} />
+            {Current && <Current localizer={globalizeLocalizer} />}
           </div>
         </div>
         <div className="jumbotron">
           <div className="container">
-            <h1>
-          
-            </h1>
           </div>
         </div>
       </div>
     )
-  }
+  // }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
